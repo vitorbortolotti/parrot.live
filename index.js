5d161ad1554f6f6f48f63383d2790319734a7ec2
Vitor Bortolotti <bortolottiv.vitor@gmail.com>
2019-11-04 17:27:36 -0300

    Changing server port

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const port = process.env.PARROT_PORT || 80;
+const port = process.env.PORT || 3000;
 server.listen(port, err => {
   if (err) throw err;
   console.log(`Listening on localhost:${port}`);
